f3855d52f55037b85df32da8f5c9465db688b05c
FionaAvila <fionaavila15@gmail.com>
2020-10-17 13:54:22 +0700

    Initial Commit

---
+++ src/if1/pkg10119013/latihan7/integer/if110119013Latihan7Integer.java
@@ -0,0 +1,38 @@
+/*
+ * NAMA  : Fiona Avila Putri
+ * NIM   : 10119013
+ * KELAS : IF1 2019/2020 (PBO1)
+ */
+
+
+
+package if1.pkg10119013.latihan7.integer;
+
+
+/**
+ *
+ * @author Fiona Avila
+ */
+
+
+public class if110119013Latihan7Integer {
+
+    public static void main(String[] args) {
+        
+        byte b;
+        short s;
+        int i;
+        long l;
+        
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        
+        System.out.println("byte\t: " + b);
+        System.out.println("short\t: " + s);
+        System.out.println("int\t: " + i);
+        System.out.println("long\t: " + l);
+    }
+    
+}
